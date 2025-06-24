deb2f674fe8f2308b419997256792568573e7c49
Terry Kernan <tkernan@safetynetaccess.com>
2025-06-24 14:55:23 +0100

    added http pap and other methods to authentication

--- mikrotik/reconfig.py
+++ mikrotik/reconfig.py
@@ -151,10 +151,10 @@ def configure_mikrotik(ip: str, port: int, username: str, password: str, portal_
             exec(f"ip service set ssh address={updated_acl_list}")
 
         # Create a new RADIUS profile
-        exec(f"radius add service=hotspot,login address={radius_ingress} secret={radius_secret} authentication=pap,chap,http-cookie comment=SN_CHOICE")
+        exec(f"radius add service=hotspot,login address={radius_ingress} secret={radius_secret} comment=SN_CHOICE")
 
         # Switch the server profile to the `sn_choice` profile
-        exec("ip hotspot profile set [find name=sn_choice] use-radius=yes")
+        exec("ip hotspot profile set [find name=sn_choice] use-radius=\"yes\" login-by=\"cookie,http-chap,http-pap\"")
 
         # Switch system to use the new RADIUS profile
         radius_to_retain = ""  # Don't disable this RADIUS profile
